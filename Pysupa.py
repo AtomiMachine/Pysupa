f3bbdbdc19337ac6158e3f9f86b9ce67693a00d4
ATOM <atombusi@gmail.com>
2024-05-28 16:14:45 +0900

    whisper_set

--- Pysupa.py
+++ Pysupa.py
@@ -6,6 +6,8 @@ import webbrowser
 import threading
 from tkinter import filedialog
 import os
+import whisper
+
 # -----------
 # import 
 # -----------
@@ -14,3 +16,10 @@ app = Flask(__name__)
 filetype = [("Audiofile",".mp3 .wav")]
 path = os.path.abspath(os.path.dirname(__file__))
 cap_filepath = filedialog.askopenfilename(filetype = filetype, initialdir = path)
+
+model = whisper.load_model("base")
+result = model.transcribe(cap_filepath)
+print(result["text"])
+
+# @app.route('./')
+
