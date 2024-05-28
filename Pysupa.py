2d8c79606ed4105d11febcc83501bf1ff13cc8cd
ATOM <atombusi@gmail.com>
2024-05-28 15:44:11 +0900

    explorer_up

--- Pysupa.py
+++ Pysupa.py
@@ -9,3 +9,8 @@ import os
 # -----------
 # import 
 # -----------
+app = Flask(__name__)
+
+filetype = [("Audiofile",".mp3 .wav")]
+path = os.path.abspath(os.path.dirname(__file__))
+cap_filepath = filedialog.askopenfilename(filetype = filetype, initialdir = path)
