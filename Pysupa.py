50b4a377180eacad699c2ab6e47f06e2e4aa4a99
ATOM <atombusi@gmail.com>
2024-06-04 09:24:01 +0900

    exe作成

--- Pysupa.py
+++ Pysupa.py
@@ -15,7 +15,9 @@ app = Flask(__name__)
 
 filetype = [("Audiofile",".mp3 .wav")]
 path = os.path.abspath(os.path.dirname(__file__))
+# cap_filepath = filedialog.askopenfilename(filetype = filetype, initialdir = path)
 cap_filepath = filedialog.askopenfilename(filetype = filetype, initialdir = path)
+
 
 model = whisper.load_model("base")
 result = model.transcribe(cap_filepath)
