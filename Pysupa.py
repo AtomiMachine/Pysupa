0416a2de094037d2c1594a6ae9a0dfcdb12d2ac3
ATOM <atombusi@gmail.com>
2024-06-05 09:36:16 +0900

    gitignore_venv追加

--- Pysupa.py
+++ Pysupa.py
@@ -4,6 +4,7 @@
 from flask import Flask
 import webbrowser
 import threading
+import tkinter as tk
 from tkinter import filedialog
 import os
 import whisper
@@ -13,19 +14,19 @@ import whisper
 # -----------
 app = Flask(__name__)
 
+root = tk.Tk()
+root.attributes("-topmost", True)
+root.mainloop()
+
 filetype = [("Audiofile",".mp3 .wav")]
 path = os.path.abspath(os.path.dirname(__file__))
 # cap_filepath = filedialog.askopenfilename(filetype = filetype, initialdir = path)
 cap_filepath = filedialog.askopenfilename(filetype = filetype, initialdir = path)
-
-
-model = whisper.load_model("base")
-result = model.transcribe(cap_filepath)
+print("quit")
+#model = whisper.load_model("base")
+#result = model.transcribe(cap_filepath)
 # file_object = open("audio_to_textfile","base")
 
-print(result["text"])
+#print(result["text"])
 
 # @app.route('./')
-
-
-
